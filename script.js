62fdb3e16a024b95d3ebb54b24fe2cd26ed5f2cd
Sayed Haamid Tore <toreshcoding@gmail.com>
2022-02-08 17:38:05 -0500

    began adding operator functions and finished button layout

---
+++ script.js
@@ -0,0 +1,47 @@
+// function mathFunc() {
+//   let arr = [];
+// }
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    return "ERROR!";
+  } else {
+    return a / b;
+  }
+}
+
+function power(a, b) {
+  return Math.pow(a, b);
+}
+
+function operate(operator, a, b) {
+  switch(operator){
+    case '+':
+      return add(a, b);
+      break;
+    case '-':
+      return subtract(a, b);
+      break;
+    case '*':
+      return multiply(a, b);
+      break;
+    case '/':
+      return divide(a, b);
+      break;
+    case '^':
+      return power(a, b);
+      break;
+  }
+}
