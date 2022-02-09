83eee09aace9dae69d341d5f3192c288e30b107c
Sayed Haamid Tore <toreshcoding@gmail.com>
2022-02-09 18:30:51 -0500

    adding DOM for buttons

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+// const clearBtn = document.getElementById('clear-btn');
+// const delBtn = document.getElementById('delete-btn');
+// const powBtn = document.getElementById('power-btn');
+// const btn7 = document.getElementById('7-btn');
+// const btn8 = document.getElementById('8-btn');
+// const btn9 = document.getElementById('9-btn');
+// const divBtn = document.getElementById('div-btn');
+// const btn4 = document.getElementById('4-btn');
+// const btn5 = document.getElementById('5-btn');
+// const btn6 = document.getElementById('6-btn');
+// const multBtn = document.getElementById('mult-btn');
+// const btn1 = document.getElementById('1-btn');
+// const btn2 = document.getElementById('2-btn');
+// const btn3 = document.getElementById('3-btn');
+// const subBtn = document.getElementById('sub-btn');
+// const decBtn = document.getElementById('dec-btn');
+// const btn0 = document.getElementById('0-btn');
+// const equalsBtn = document.getElementById('eq-btn');
+// const addBtn= document.getElementById('add-btn');
+const clearBtn = document.querySelector('[data-clear-all]');
+const delBtn = document.querySelector('[data-delete]');
+const numBtns = document.querySelectorAll('[data-number]');
+const operatorBtns = document.querySelectorAll('[data-operation]');
+const equealsBtn = document.querySelector('[data-equals]');
+
+
 // function mathFunc() {
 //   let arr = [];
 // }
